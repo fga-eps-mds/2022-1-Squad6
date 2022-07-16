5d3570a2c00df396ce162eca53befe74a2832629
hannanhunny01 <hannanhoney5000@gmail.com>
2022-07-15 23:01:39 -0300

    adding splash with scrapy

--- cebraspeCrawler/cebraspeCrawler/spiders/cebraspe.py
+++ cebraspeCrawler/cebraspeCrawler/spiders/cebraspe.py
@@ -1,10 +1,25 @@
 import scrapy
-
+from scrapy_splash import SplashRequest
 
 class CebraspeSpider(scrapy.Spider):
     name = 'cebraspe'
-    allowed_domains = ['www.cebraspe.org.br']
-    start_urls = ['http://www.cebraspe.org.br/']
+    allowed_domains = ['https://www.cebraspe.org.br']
+    
+    script ='''
+    function main(splash, args)
+  assert(splash:go(args.url))
+  assert(splash:wait(0.5))
+  return {
+    html = splash:html(),
+    png = splash:png(),
+    har = splash:har(),
+  }
+end
+    
+    '''
+    def start_requests(self):
+        yield SplashRequest(url="https://www.cebraspe.org.br/pas/subprogramas/2019_2021/3",callback=self.parse,endpoint="execute",args={'lua_source':self.script})
+
 
     def parse(self, response):
-        pass
+        print(response.body)
