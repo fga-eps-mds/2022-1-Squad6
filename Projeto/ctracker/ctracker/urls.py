e2e825aa58b9ad89dda9a45358c3f2c07a08d968
hackairos <joao.brasilpv@gmail.com>
2022-07-20 22:00:05 -0300

    alunos

--- Projeto/ctracker/ctracker/urls.py
+++ Projeto/ctracker/ctracker/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from alunos.api import viewsets as alunosviewSet
+
+route = routers.DefaultRouter()
+
+route.register(r'alunos/', alunosviewSet.AlunosViewSet, basename="Alunos")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(route.urls))
 ]
