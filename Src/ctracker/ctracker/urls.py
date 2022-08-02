59f7baa10ee76d8bb1926dbe7144521565d8adc2
HacKairos <joao.brasilpv@gmail.com>
2022-08-01 22:23:36 -0300

    Better code hub

--- Src/ctracker/ctracker/urls.py
+++ Src/ctracker/ctracker/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework import routers
-
 from alunos.api import viewsets as alunosviewSet
-
 route = routers.DefaultRouter()
-
 route.register(r'alunos', alunosviewSet.AlunosViewSet, basename="Alunos")
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include(route.urls))
-]
+urlpatterns = [path('admin/', admin.site.urls),path('',include(route.urls))]
