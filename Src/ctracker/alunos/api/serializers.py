d87f765c3e7d27c106486313c002ea7812181dea
HacKairos <joao.brasilpv@gmail.com>
2022-09-03 02:32:32 -0300

    Reorganização do repositorio para documentação

--- Src/ctracker/alunos/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from alunos import models
-
-class AlunosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.alunos
-        fields = '__all__'
--- Src/ctracker/alunos/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from uuid import uuid4
-
-# Create your models here.
-
-class alunos(models.Model):
-    aluno_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    nome = models.CharField(max_length=255)
-    inscricao = models.IntegerField()
